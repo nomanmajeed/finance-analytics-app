47d191a751383f08b269d8f45986ba31874e3b32
nomanmajeed <nomanmajeed986@gmail.com>
2023-03-27 02:05:03 +0500

    Navbar | app logo color updated

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -13,7 +13,7 @@ const Navbar = (props: Props) => {
     <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
       {/* LEFT SIDE */}
       <FlexBetween gap="0.75rem">
-        <PixIcon sx={{ fontSize: "28px" }} />
+        <PixIcon sx={{ fontSize: "28px", color: "#4BB8A9" }} />
         <Typography variant="h4" fontSize="16px">
           Treasury Analytics
         </Typography>
