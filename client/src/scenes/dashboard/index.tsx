ef3c82a31cb5b20c5321f2007890f0fdb8890e10
nomanmajeed <nomanmajeed986@gmail.com>
2023-03-27 02:54:29 +0500

    Dashbaord grid layout for small and medium screens added

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 
 const gridTemplateLargeScreens = `
   "a b c"
@@ -13,19 +13,61 @@ const gridTemplateLargeScreens = `
   "g h j"
 `;
 
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`;
+
 const Dashboard = () => {
-  return (
-    <Box
-      width="100%"
-      height="100%"
-      display="grid"
-      gap="1.5rem"
-      sx={{
-              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-              gridTemplateAreas: gridTemplateLargeScreens,
-            }}
-    >
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+    return (
+      <Box
+        width="100%"
+        height="100%"
+        display="grid"
+        gap="1.5rem"
+        sx={
+          isAboveMediumScreens
+            ? {
+                gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+                gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+                gridTemplateAreas: gridTemplateLargeScreens,
+              }
+            : {
+                gridAutoColumns: "1fr",
+                gridAutoRows: "80px",
+                gridTemplateAreas: gridTemplateSmallScreens,
+              }
+        }
+      >
       <Box bgcolor="#fff" gridArea="a"></Box>
       <Box bgcolor="#fff" gridArea="b"></Box>
       <Box bgcolor="#fff" gridArea="c"></Box>
