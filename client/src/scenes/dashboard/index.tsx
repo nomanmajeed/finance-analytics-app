96684bbdbe0a57a2057b2dbe872d90b7cde4a091
nomanmajeed <nomanmajeed986@gmail.com>
2023-03-27 20:08:04 +0500

    Dashboard layout splitted into individual Row components

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,5 +1,8 @@
 import DashboardBox from "@/components/DashboardBox";
 import { Box, useMediaQuery } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 const gridTemplateLargeScreens = `
   "a b c"
@@ -48,37 +51,30 @@ const gridTemplateSmallScreens = `
 `;
 
 const Dashboard = () => {
-    const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
-    return (
-      <Box
-        width="100%"
-        height="100%"
-        display="grid"
-        gap="1.5rem"
-        sx={
-          isAboveMediumScreens
-            ? {
-                gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-                gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-                gridTemplateAreas: gridTemplateLargeScreens,
-              }
-            : {
-                gridAutoColumns: "1fr",
-                gridAutoRows: "80px",
-                gridTemplateAreas: gridTemplateSmallScreens,
-              }
-        }
-      >
-      <DashboardBox bgcolor="#fff" gridArea="a"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="b"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="c"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="d"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="e"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="f"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="g"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="h"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="i"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="j"></DashboardBox>
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
+    >
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   );
 };
