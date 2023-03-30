de872ba28a12af3325594ccc39bdd10949984a34
nomanmajeed <nomanmajeed986@gmail.com>
2023-03-31 04:49:41 +0500

    MongoDB Cluster connected and express app listener added

--- server/index.js
+++ server/index.js
@@ -22,4 +22,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-console.log("HELLO")
+
+console.log(process.env.MONGO_URL);
+console.log(process.env.PORT);
+/* MONGOOSE SETUP and App listener */
+
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
