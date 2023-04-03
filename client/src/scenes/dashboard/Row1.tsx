3bbcab579a3f1d90abb18e30936625ff7d42c4f8
nomanmajeed <nomanmajeed986@gmail.com>
2023-04-03 15:43:30 +0500

    Revenue data destructured map map AreaCharts data

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,10 +1,38 @@
 import DashboardBox from "@/components/DashboardBox";
 import { useGetKpisQuery } from "@/state/api";
+// import { useTheme } from "@mui/material";
+import { useMemo } from "react";
+
 
 type Props = {};
 
 const Row1 = (props: Props) => {
   const { data } = useGetKpisQuery();
+
+  const revenue = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+        };
+      })
+      );
+    }, [data]);
+
+  const revenueExpenses = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3).charAt(0).toUpperCase() + month.substring(0, 3).slice(1),
+          revenue: revenue,
+          expenses: expenses,
+        };
+      })
+      );
+    }, [data]);
 
   return (
     <>
