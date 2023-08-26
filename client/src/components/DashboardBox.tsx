02f0dbe7b3e86745cfc27fca32cb6758e20e23c7
nomanmajeed <nomanmajeed986@gmail.com>
2023-08-26 23:46:22 +0500

    box container border radius updated

--- client/src/components/DashboardBox.tsx
+++ client/src/components/DashboardBox.tsx
@@ -3,7 +3,7 @@ import { styled } from "@mui/system";
 
 const DashboardBox = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.background.light,
-  borderRadius: "1rem",
+  borderRadius: "0.5rem",
   boxShadow: "0.15rem 0.2rem 0.15rem 0.1rem rgba(0, 0, 0, .8)",
 }));
 
